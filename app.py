d9672f1fbb5f95895fafce9eb12cceadba782731
Mike Rapa <mike@mikerapa.com>
2022-09-22 12:02:52 -0400

    Added Mens and Womens methods

--- app.py
+++ app.py
@@ -5,24 +5,23 @@ def get_wc_data() -> list[dict]:
     data = json.load(open("worldcupdata.json", "r"))
     return data
 
-
-def show_list(winners: list[dict]):
+def display_winners(winners : list[dict]):
+    def format_competition_name(competition:str):
+        return "Men's FIFA World Cup" if competition=="men" else "Women's FIFA World Cup"
     for winner in winners:
-        print(f"{winner['country']} \t {winner['competition']} - {winner['year']}")
+        print(f"{winner['country']} won the {format_competition_name(winner['competition'])} in {winner['year']}")
 
 
-def get_womens_winners_by_country(country_name: str) -> list[dict]:
-    data: list[dict] = get_wc_data()
-    return [cup for cup in data if cup['country'] == country_name and cup['competition'] == "women"]
+def get_womens_winners_by_country(country: str, winners:list[dict]):
+    filtered_data : list[dict] = []
+    for winner in winners:
+        if winner['competition'] == "women" and winner['country']==country:
+            filtered_data.append(winner)
 
-
-def get_mens_winners_by_country(country_name: str) -> list[dict]:
-    data: list[dict] = get_wc_data()
-    return [cup for cup in data if cup['country'] == country_name and cup['competition'] == "men"]
+    return filtered_data
 
 
 if __name__ == '__main__':
-    winners = get_womens_winners_by_country("Germany")
-    show_list(winners)
-    mens_winners = get_mens_winners_by_country("Brazil")
-    show_list(mens_winners)
+    wc_data = get_wc_data()
+    womens_winners = get_womens_winners_by_country("United States", wc_data)
+    display_winners(womens_winners)
