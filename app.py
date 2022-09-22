8e14dc25d1d60aefea84f8ee65a22c69a1755bf1
Mike Rapa <mike@mikerapa.com>
2022-09-22 12:14:17 -0400

    Added rich

--- app.py
+++ app.py
@@ -1,21 +1,25 @@
 import json
+from rich import print
 
 
 def get_wc_data() -> list[dict]:
     data = json.load(open("worldcupdata.json", "r"))
     return data
 
-def display_winners(winners : list[dict]):
-    def format_competition_name(competition:str):
-        return "Men's FIFA World Cup" if competition=="men" else "Women's FIFA World Cup"
+
+def display_winners(winners: list[dict]):
+    def format_competition_name(competition: str):
+        return "Men's FIFA World Cup" if competition == "men" else "Women's FIFA World Cup"
+
     for winner in winners:
-        print(f"{winner['country']} won the {format_competition_name(winner['competition'])} in {winner['year']}")
+        print(
+            f"{winner['country']} won the {format_competition_name(winner['competition'])} in [blue]{winner['year']}[/blue]")
 
 
-def get_womens_winners_by_country(country: str, winners:list[dict]):
-    filtered_data : list[dict] = []
+def get_womens_winners_by_country(country: str, winners: list[dict]):
+    filtered_data: list[dict] = []
     for winner in winners:
-        if winner['competition'] == "women" and winner['country']==country:
+        if winner['competition'] == "women" and winner['country'] == country:
             filtered_data.append(winner)
 
     return filtered_data
